2e25ab87f0868f5e0ed05891f2e3b4ffebd6f6bc
Maytham Ghaly <maythamghaly@gmail.com>
2022-09-16 14:27:32 +0300

    add-cards

---
+++ javascript/javascript.js
@@ -0,0 +1,9 @@
+window.onload = () => {
+    const cards = document.querySelectorAll(".card")
+    
+    cards.forEach((card) => {
+        card.addEventListener('click', function () {
+            card.classList.toggle('flipcard');
+        });
+    });
+}
