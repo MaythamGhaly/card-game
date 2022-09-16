3d6885b218ed45567cd1f49e2e10b2ddf7d0d41e
Maytham Ghaly <maythamghaly@gmail.com>
2022-09-16 16:20:57 +0300

    add-shuffle-and-flip-fct

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,9 +1,33 @@
 window.onload = () => {
-    const cards = document.querySelectorAll(".card")
-    
-    cards.forEach((card) => {
-        card.addEventListener('click', function () {
-            card.classList.toggle('flipcard');
+    const cards = document.querySelectorAll('.card');
+    const sc = document.getElementById("score")
+    let score = 0
+    let hasFlippedCard = false;
+    let lockBoard = false;
+    let firstCard, secondCard;
+
+    cards.forEach(card => card.addEventListener('click', flipCard));
+    // function to shuffle the cardes according to the order
+    (function shuffle() {
+        cards.forEach(card => {
+            let randomPos = Math.floor(Math.random() * 6);
+            card.style.order = randomPos;
         });
-    });
+    })();
+    // function to flip the card 
+    function flipCard() {
+        if (lockBoard) return;
+        if (this === firstCard) return;
+        this.classList.add('flipcard');
+        if (!hasFlippedCard) {
+            hasFlippedCard = true;
+            firstCard = this;
+            return;
+        }
+        secondCard = this;
+        checkForMatch();
+    }
+    
+    
+    
 }
